daaa76f565a3cd9f8d2c3063241bfa4a564477b2
Luigi Menegolo <92937717+gigimen@users.noreply.github.com>
2022-01-19 09:09:06 +0100

    tutto ok

--- Stored Procedures/Accounting.usp_DeleteLifeCycleProgress.sql
+++ Stored Procedures/Accounting.usp_DeleteLifeCycleProgress.sql
@@ -76,10 +76,13 @@ set @statetimeLoc = GeneralPurpose.fn_UTCToLocal(1,@statetimeUTC)
 declare @attr varchar(1024)	
 select @attr = 'LifeCycleID=''' + cast(LifeCycleID as varchar(16)) + ''' ' +
 		    'StockID=''' 	 + cast(StockID as varchar(16)) + ''' ' +
+		    'StockTypeID=''' 	 + cast(StockTypeID as varchar(16)) + ''' ' +
+		    'Tag=''' 	 + Tag + ''' ' +
+			'GamingDate=''' +  [GeneralPurpose].[fn_CastDateForAdoRead](GamingDate) + ''' ' +			
 		    'DenoID = '''    + cast(@DenoID as varchar(16)) + ''' ' +
 		    'TransTimeUTC='''+ cast(DatePArt(hh,@stateTimeUTC) as varchar(16))  + ''' ' +
 		    'TransTimeLoc='''+ cast(DatePArt(hh,@stateTimeLOC) as varchar(16))  + ''''
-from Accounting.tbl_LifeCycles 
+from Accounting.vw_AllStockLifeCycles 
 where LifeCycleID = @lfCyID
 	
 execute [GeneralPurpose].[usp_BroadcastMessage] 'DeleteProgress',@attr
